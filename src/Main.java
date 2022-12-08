4177c160ae22c1ebd792eadf8baf782cee818874
crazy <crazy303@mail.ru>
2022-12-08 20:14:38 +0500

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,60 @@
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       int year = 2019;
+        System.out.println(LeapYear(year));
+        int clientOS = 1;
+        int clientDeviceYear = 2014;
+        System.out.println(client(clientOS, clientDeviceYear));
+        int deliveryDay = 1;
+        int deliveryDistance = 95;
+        System.out.println(delivery(deliveryDay, deliveryDistance));
     }
-}
+
+    public static int LeapYear(int y)
+    {
+        if (y % 4 <= 0 && y % 100 != 0 || y % 400 <= 0)
+        {System.out.println(y + " год является високосным");}
+        else {System.out.println(y + " год не является високосным");}
+
+        return y;
+    }
+    public static int client(int x, int z){
+
+    if (x == 0) {
+        if (z < 2015) {
+            System.out.println("Установитe облегченную версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+    } else {
+        if (z < 2015) {
+            System.out.println("Установитe облегченную версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+    }
+        return x;
+    }
+    public static int delivery(int x, int y){
+    if (y > 20) {
+        x++;
+    }
+    if (y > 60) {
+        x++;
+    }
+    System.out.println("потребуется дней: " + x);
+        return x;
+    }}
+
+
+
+
+
+
+
+
+
+
+
+
